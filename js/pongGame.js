7daadb2e16ec5db00d06c268529eb2bd066bf72b
cb <csbutton01@hotmail.com>
2018-06-15 10:56:46 -0700

    keep working on pong game and taking notes to understand concepts covered

--- js/pongGame.js
+++ js/pongGame.js
@@ -3,15 +3,27 @@ let ballX = 50;
 let ballY = 50;
 let ballWidth = 25;
 let ballHeight = 25;
+let ballSpeedX = 10;
+
 
 window.onload = function(){
   canvas = document.getElementById('pong-canvas'); //gets the canvas element from the dom by the id of pong-game
   canvasContext = canvas.getContext('2d');  //after looking at the getContext() method on mdn I now understand that this method returns a drawing context on the canvas
-  drawEverything(); //draws everything inside the drawEverything() function
+  let fps = 1000/60; // I don't want the fps to be changed by anything in the program, so it will be limited to the scope of window.onload
+  setInterval(function(){ //an update all function isn't necessary since it wont be changed later
+    drawEverything();
+    moveEverything();
+  }, fps);
 }
+
+
 
 function drawEverything(){
   drawRect(0,0, canvas.width, canvas.height, 'black');  //defines a rectangle with the drawRect() function
+}
+
+function moveEverything() {
+
 }
 
 function drawRect(topX, topY, width, height, color) { // dry up code and make it reusable by making the fillRect() method into a helper function
